d23194e8d16278cc1ae28daa2170830b3ec9ff2b
hamnakashif <hamnakashif397@gmail.com>
2025-02-08 18:45:25 +0500

    hiding sensitive data

--- lib/u_auth/firebase_auth/firestore_service.dart
+++ lib/u_auth/firebase_auth/firestore_service.dart
@@ -21,4 +21,25 @@ class FirestoreService {
       throw Exception("User not found");
     }
   }
+
+  Future<void> createUserInFirestore(String name, String birthdate, String relation, String email, String gender) async {
+    User? user = _auth.currentUser;
+    if (user != null) {
+      try {
+        await _db.collection('users').doc(user.uid).set({
+          'name': name,
+          'birthdate': birthdate,
+          'relation': relation,
+          'email': email,
+          'gender': gender,
+          'uid': user.uid,
+          'createdAt': FieldValue.serverTimestamp(),
+        });
+      } catch (e) {
+        throw Exception("Error adding user to Firestore: $e");
+      }
+    } else {
+      throw Exception("User not found");
+    }
+  }
 }
