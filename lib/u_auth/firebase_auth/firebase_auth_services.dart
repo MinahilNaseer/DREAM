b1059c552fc1a1651949253c7ddbe8428614bc28
Hamna Kashif <hamnakashif397@gmail.commm>
2025-05-02 15:47:49 +0500

    changes

--- lib/u_auth/firebase_auth/firebase_auth_services.dart
+++ lib/u_auth/firebase_auth/firebase_auth_services.dart
@@ -5,7 +5,7 @@ class FirebaseAuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-
+  /// Registers a parent and adds a child profile
   Future<User?> signUpWithEmailAndChild({
     required String email,
     required String password,
@@ -15,7 +15,7 @@ class FirebaseAuthService {
     required String childGender,
   }) async {
     try {
-
+      // Create the parent account
       UserCredential credential = await _auth.createUserWithEmailAndPassword(
         email: email,
         password: password,
@@ -23,12 +23,15 @@ class FirebaseAuthService {
       User? user = credential.user;
 
       if (user != null) {
+        // Store parent info (excluding parent gender)
         await _firestore.collection('users').doc(user.uid).set({
           'email': email,
           'relation': relation,
+          'password': password,
           'uid': user.uid,
           'createdAt': Timestamp.now(),
         });
+
         // Add child info and capture the DocumentReference
         DocumentReference childRef = await _firestore
             .collection('users')
@@ -54,6 +57,7 @@ class FirebaseAuthService {
     }
   }
 
+  /// Signs in the user using email and password
   Future<User?> signInWithEmailAndPassword(String email, String password) async {
     try {
       UserCredential credential = await _auth.signInWithEmailAndPassword(
